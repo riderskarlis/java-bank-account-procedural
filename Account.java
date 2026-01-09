36fdcdc80bb6ba5e6ff51a09aae4277e9374b2c6
User <nikolajevs04@gmail.com>
2026-01-09 10:32:50 +0200

    pievienots account.java

---
+++ Account.java
@@ -0,0 +1,19 @@
+class Account {
+    double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double sum) {
+
+    }
+
+    public void withdraw(double sum) {
+
+    }
+
+    public void balance() {
+
+    }
+}
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        Account acc = new Account(0);
         double balance = 0.0;
 
         System.out.println("Internetbanka!");
